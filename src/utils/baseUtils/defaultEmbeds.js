a19c0a84d8b2cddfe0b5f80f6d0c8d702ef1e88c
SixieP <SixieP@sixie.nl>
2023-08-25 13:33:54 +0200

    added title logic

--- src/utils/baseUtils/defaultEmbeds.js
+++ src/utils/baseUtils/defaultEmbeds.js
@@ -1,20 +1,36 @@
 const { EmbedBuilder } = require("discord.js");
 
 
-function deniedMessage(title, description) {
-    const noPermsEmbed = new EmbedBuilder()
-    .setColor(0xC83400)
-    .setTitle(title)
-    .setDescription(`:x: ${description}`);
+function deniedMessage(description, title) {
+
+    var noPermsEmbed
+    if (title) {
+        noPermsEmbed = new EmbedBuilder()
+        .setColor(0xC83400)
+        .setTitle(title)
+        .setDescription(`:x: ${description}`);
+    } else {
+        noPermsEmbed = new EmbedBuilder()
+        .setColor(0xC83400)
+        .setDescription(`:x: ${description}`);
+    }
 
     return noPermsEmbed;
 }
 
-function aprovedMessage(title, description) {
-    const doneEmbed = new EmbedBuilder()
-    .setColor(0xB000)
-    .setTitle(title)
-    .setDescription(`:white_check_mark: ${description}`);
+function aprovedMessage(description, title) {
+
+    var doneEmbed;
+    if (title) {
+        doneEmbed = new EmbedBuilder()
+        .setColor(0xB000)
+        .setTitle(title)
+        .setDescription(`:white_check_mark: ${description}`);
+    } else {
+        doneEmbed = new EmbedBuilder()
+        .setColor(0xB000)
+        .setDescription(`:white_check_mark: ${description}`);
+    }
 
     return doneEmbed;
 }
