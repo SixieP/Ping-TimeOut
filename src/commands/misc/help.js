39aaf1773217fa9c339d06675ae1190813b32d8d
SixieP <sixiep@sixie.nl>
2023-08-31 13:16:43 +0200

    changed perms, changed text

--- src/commands/misc/help.js
+++ src/commands/misc/help.js
@@ -3,7 +3,6 @@ const { deniedMessage } = require('../../utils/baseUtils/defaultEmbeds');
 module.exports = {
     name: "help",
     description: 'Use this command if you need help with the bot',
-    defaultMemberPermissions: PermissionFlagsBits.Administrator,
     options: [
         {
             name: "page",
@@ -35,22 +34,23 @@ module.exports = {
             .setDescription(`
             ${bold("All commands")}
             ${underscore("Setup")}
-            Use ${inlineCode("/timeout-check")} to check for all roles that you can add a timeout to. Then use ${inlineCode("/timeout-role add")} to add a timeout to a role.
+            ${inlineCode("/help setup")} Get the setup guide.
 
             ${underscore("Main")}
-            ${inlineCode("/timeout-role add")} Add a timeout to a role that doesn't have one already.
-            ${inlineCode("/timeout-role edit")} Edit the timeout duration of a role that already has one.
-            ${inlineCode("/timeout-role remove")} Remove a timeout from a role that has one.
+            ${inlineCode("/timed-role add")} Add a timeout to a role that doesn't have one already.
+            ${inlineCode("/timed-role edit")} Edit the timeout duration of a role that already has one.
+            ${inlineCode("/timed-role remove")} Remove a timeout from a role that has one.
 
             ${underscore("Misc")}
-            ${inlineCode("/timeout-check")} Get a list of all the roles that you can add a timeout to. It also shows what roles already have a timeout enabled.
+            ${inlineCode("/check")} Get a list of all the roles that you can add a timeout to. It also shows what roles already have a timeout enabled.
             ${inlineCode("/ping")} Get the bots ping. You can also use this to check if the bot is still working when other commands don't work correctly.
 
             ${underscore("Help")}
-            ${inlineCode("/help")} Gives the default help page.
-            ${inlineCode("/help 1")} Gives help page 1 (default).
-            ${inlineCode("/help setup")} Gives you the bot setup guide.
+            ${inlineCode("/help")} Get the default help page.
+            ${inlineCode("/help 1")} Get help page 1 (default).
+            ${inlineCode("/help setup")} Get the setup guide.
 
+            *For other help pages use the optional page on the help command*
             `)
         }
 
@@ -58,12 +58,14 @@ module.exports = {
             embed = new EmbedBuilder()
             .setTitle("Ping TimeOut | Setup")
             .setDescription(`
-            **1** - Use ${inlineCode("/timeout-check")} to check for all roles that you can add a timeout to. 
-            **2** - Use ${inlineCode("/timeout-role add")} to add a timeout to a role.
+            **Important**: You need to have the __Administrator__ permission for most of the commands. (Administrators can change slash command permissions in the intergration settings)
+            
+            **1** - Use ${inlineCode("/check")} to check for all roles that you can add a timeout to. 
+            **2** - Use ${inlineCode("/timed-role add")} to add a timeout to a role.
             
             **3** - You can check if the bot work by following one of these steps
             **3a** - Use a account that doesn't have the permission to ping @everyone and ping the role. Then check if they can ping the role directly again.
-            **3b/he** - Check in ${inlineCode("Server Settings -> Audit Log")} if the bot updated the role to Mentionable.
+            **3b** - Check in ${inlineCode("Server Settings -> Audit Log")} if the bot updated the role to Mentionable.
             `)
         }
 
