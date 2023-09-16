b5d04e246bc8557fe1f85b77b52b99728f0f5dc6
SixieP <sixiep@sixie.nl>
2023-09-17 01:46:57 +0200

    added extra logging logic

--- src/utils/baseUtils/logging.js
+++ src/utils/baseUtils/logging.js
@@ -1,11 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 
+const { fullLogging } = require('../../../config.json');
+
 
 // info types: INFO, WARNING, ERROR, DEV
 
 
-function logging(type, message, subtype, logfile=false) {
+function logging(type, message, subtype, logfileOnly=false) {
     const now = new Date();
     const time = `${now.getUTCHours()}:${now.getUTCMinutes()}:${now.getUTCSeconds()}-${now.getUTCDay()}/${now.getUTCMonth()+1}/${now.getUTCFullYear()}`
     
@@ -20,28 +22,43 @@ function logging(type, message, subtype, logfile=false) {
             logOut = `[${time} | ${type.toUpperCase()}] ${message}`;
         }
     }
-    console.log(logOut);
 
     const daydate = `${now.getUTCDay()}-${now.getUTCMonth()+1}-${now.getUTCFullYear()}`
+
+    //only log to console if it is NOT log file only
+    if (!logfileOnly) {
+        console.log(logOut);
+
+        const logLocation = path.join(__dirname, '..', '..', '..', 'logs', `${daydate}.txt`)
+
+        fs.appendFile(logLocation, `${logOut}\n`, (err) => {
+            if (err) throw err;
+        });
+    } else {
+        if (!fullLogging) return;
+
+        const logLocation = path.join(__dirname, '..', '..', '..', 'logs', `logfileOnly_${daydate}.txt`)
+
+        fs.appendFile(logLocation, `${logOut}\n`, (err) => {
+            if (err) throw err;
+        });
+    }
+
     
-    const logLocation = path.join(__dirname, '..', '..', '..', 'logs', `${daydate}.txt`)
-    fs.appendFile(logLocation, `${logOut}\n`, (err) => {
-        if (err) throw err;
-    })
 
     if (type === "error") {
         const logLocation = path.join(__dirname, '..', '..', '..', 'logs', `error_${daydate}.txt`)
         fs.appendFile(logLocation, `${logOut}\n`, (err) => {
-        if (err) throw err;
-    })
-    }   
+            if (err) throw err;
+        });
+    };  
     if (type === "warning") {
         const logLocation = path.join(__dirname, '..', '..', '..', 'logs', `warning_${daydate}.txt`)
         fs.appendFile(logLocation, `${logOut}\n`, (err) => {
-        if (err) throw err;
-    })
-    }
-}
+            if (err) throw err;
+        });
+    };
+};
 
 module.exports = { 
     logging,
