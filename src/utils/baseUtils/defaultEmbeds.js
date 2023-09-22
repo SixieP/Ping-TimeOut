3038d47774acf898f13f969d2ec40e5bbd524982
SixieP <sixiep@sixie.nl>
2023-09-22 21:48:59 +0200

    created a default warn message

--- src/utils/baseUtils/defaultEmbeds.js
+++ src/utils/baseUtils/defaultEmbeds.js
@@ -34,7 +34,25 @@ function aprovedMessage(description, title) {
     return doneEmbed;
 }
 
+function warnMessage(description, title) {
+
+    var warnEmbed;
+    if (title) {
+        warnEmbed = new EmbedBuilder()
+        .setColor(0xf0b616)
+        .setTitle(title)
+        .setDescription(`:warning: ${description}`);
+    } else {
+        warnEmbed = new EmbedBuilder()
+        .setColor(0xf0b616)
+        .setDescription(`:warning: ${description}`);
+    }
+
+    return warnEmbed;
+}
+
 module.exports = {
     deniedMessage,
     aprovedMessage,
+    warnMessage,
 };
