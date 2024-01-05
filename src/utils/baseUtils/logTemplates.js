5e806fe0f025d36af8f1acc9ddf8abe79f240cc7
SixieP <60109474+SixieP@users.noreply.github.com>
2024-01-05 16:15:38 +0100

    added logTemplates

--- src/utils/baseUtils/logTemplates.js
+++ src/utils/baseUtils/logTemplates.js
@@ -1,17 +1,82 @@
-exports.interactionBasic = (message, interaction) => {
+// Application Command Interactions
 
-    const JSONinfo = {'interactionId': interaction.id, 'createdAt': interaction.createdAt, 'userId': interaction.user.id, 'guildId': interaction.guildId, 'appPerms': interaction.appPermissions, 'memberPerms': interaction.memberPermissions};
+exports.commandInteractionStart = (interaction) => { // Used when an Application Command Interaction is executed
 
-    const returnMessage = `${message} | ${JSONinfo}`;
+    const info = {
+        'interactionId': interaction.id,
+        'createdAt': interaction.createdAt,
+        'userId': interaction.user.id,
+        'guildId': interaction.guildId,
+        'appPerms': interaction.appPermissions,
+        'memberPerms': interaction.memberPermissions,
+        'commandName': interaction.commandName
+    };
+
+    JSONinfo = JSON.stringify(info);
+
+    const returnMessage = `Application Slash Command has been executed | ${JSONinfo}`;
 
     return returnMessage;
 };
 
-exports.commandinteractionBasic = (message, interaction) => {
+exports.commandInteractionInfo = (interaction, event) => { // Used to give general information
 
-    const JSONinfo = {'interactionId': interaction.id, 'commandName':interaction.commandName , 'subCommand':interaction.options.getSubcommand(), 'subCommandGroup':interaction.options.getSubcommandGroup() ,'createdAt': interaction.createdAt, 'userId': interaction.user.id, 'guildId': interaction.guildId, 'appPerms': interaction.appPermissions, 'memberPerms': interaction.memberPermissions};
+    const returnMessage = `${event} | InteractionId: ${interaction.id}`;
 
-    const returnMessage = `${message} | ${JSONinfo}`;
+    return returnMessage;
+}
+
+exports.commandInteractionException  = (interaction, event, additionalData) => { // Used in both warnings and errors
+
+    const info = {
+        'interactionId': interaction.id,
+        'commandName': interaction.commandName,
+        'deferred': interaction.deferred,
+        'ephemeral': interaction.ephemeral,
+        'replied': interaction.replied,
+    };
+
+    JSONinfo = JSON.stringify(info);
+
+    var returnMessage;
+    if (additionalData) {
+        returnMessage = `Error with application command: ${event} | ${JSONinfo} | Additional Data: ${additionalData}`;
+    } else {
+        returnMessage = `Error with application command: ${event} | ${JSONinfo}`;
+    }
 
     return returnMessage;
 };
+
+exports.commandInteractionEnd = (interaction) => { // Used when an commandInteraction is done
+
+    const returnMessage = `End of Application Command Interaction | InteractionId: ${interaction.id}`;
+
+    return returnMessage;
+}
+
+exports.messageInteractionCreate = (message) => {
+    const info = {
+        'messageId': message.id,
+        'authorId': message.author.id,
+        'guildId': message.guildId
+    }
+
+    JSONinfo = JSON.stringify(info);
+
+    const logMessage = `A message was send/created | ${JSONinfo};`;
+
+    return logMessage;
+}
+
+exports.messageInteractionCustomInfo = (message, event, customInfo) => {
+
+    var logMessage;
+    if (customInfo) {
+        logMessage = `Log entry surrounding action that result by a message being created: ${event} | Message id: ${message.id} | Custom info: ${customInfo};`;
+    } else {
+        logMessage = `Log entry surrounding action that result by a message being created: ${event} | Message id: ${message.id};`;
+    }
+
+    return logMessage;
+}
