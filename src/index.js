5b1be974e3bb012503eea238074e53a2164f7324
SixieP <60109474+SixieP@users.noreply.github.com>
2024-01-05 16:09:57 +0100

    new database connection test

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const logging = require('./utils/baseUtils/logging.js');
 const { version } = require('../package.json');
 
-logging.basicInfoLog(`Starting Ping TimeOut version ${version}`)
+logging.info(__filename, `Starting Ping TimeOut version ${version}`)
 
 const { Client, IntentsBitField, Collection } = require('discord.js');
 const { botToken } = require('../config.json');
 
 const eventHandler = require('./handlers/eventHandler');
 
+
+logging.info(__filename, `Creating Client`)
 const client = new Client({
     intents: [
         IntentsBitField.Flags.Guilds,
@@ -17,32 +19,18 @@ const client = new Client({
     ]
 });
 //START check database connection
-const { mysqlHost, mysqlPort, mysqlUser, mysqlPass, mysqlDatabase } = require('../config.json');
-const mysql = require('mysql2');
-const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 
-const pool = mysql.createPool({
-    host: mysqlHost,
-    port: mysqlPort,
-    user: mysqlUser,
-    password: mysqlPass,
-    database: mysqlDatabase
-});
-pool.query(`show databases`, async function (err) {
-    if (err) {
-        logging.basicErrorLog(__filename, err, "connecting to mysql database");
+logging.info(__filename, `Testing database connection`);
 
-        await sleep(300);
+const { testConnecion } = require('./utils/database/startup/testConn.js');
 
-        throw err;
-    }
-    logging.basicInfoLog(`Succesfully connected to the database. Database: "${mysqlDatabase}"`, 'startup-database')
-})
+testConnecion();
+
 //END check database connection
 
 //create a collection that is used for command cooldowns
 client.cooldowns = new Collection();
 
 
-eventHandler(client)
+eventHandler(client);
 client.login(botToken);
