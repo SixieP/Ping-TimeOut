ac72b00939cc05f1ba55ceae6dfce2bddef6d6a0
SixieP <60109474+SixieP@users.noreply.github.com>
2024-01-07 14:16:28 +0100

    reworked command code with better logging/error handling

--- src/commands/ping-timeout/check.js
+++ src/commands/ping-timeout/check.js
@@ -3,8 +3,12 @@ const { EmbedBuilder, inlineCode } = require("@discordjs/builders");
 const { roleInDatabase } = require('../../utils/database/ping-timeout/general');
 const { PermissionFlagsBits, PermissionsBitField } = require("discord.js");
 const { permsCheck } = require("../../utils/ping-timeout/permsCheck");
-const { deniedMessage } = require('../../utils/baseUtils/defaultEmbeds');
-const { logging } = require('../../utils/baseUtils/logging')
+
+const logging = require('../../utils/baseUtils/logging');
+const logTemplates = require('../../utils/baseUtils/logTemplates');
+
+const { deniedMessage } = require("../../utils/baseUtils/defaultEmbeds");
+const defaultMessage = require('../../utils/defaults/messages/defaultMessages');
 
 module.exports = {
     name: "check",
@@ -16,93 +20,158 @@ module.exports = {
     //testCommand: Boolean,
 
     callback: async (client, interaction) => {
+        //Get the first basic vars
         const guildId = interaction.guildId;
+        const guildObject = interaction.guild;
 
-        await client.guilds.fetch();
-        await client.guilds.cache.get(guildId).roles.fetch()
-        const roles = await client.guilds.cache.get(guildId).roles.cache;
+        // Check if the guild is availabe (if the guild server isn't offline)
+        if (!guildObject.available) {
+            logging.verboseWarn(__filename, logTemplates.commandInteractionException(interaction, "Guild not availabe", `guildId: ${guildId}, errId: "err_Check_Unavail_Guild"`));
 
-        const botId = client.user.id;
-        const botUsername = client.user.username
-
-        const everyoneRole = await client.guilds.cache.get(guildId).roles.everyone
-        const everyoneId = everyoneRole.id
-
-        var roleOutput = "";
-        var trackingOutput = ""
-        var canChangeOutput = "";
-        for (const role of roles) {
-            if(role[1].id === everyoneId) {
-                if (role[1].permissions.has(PermissionFlagsBits.MentionEveryone)) {
-                    const newRolePerms = role[1].permissions.remove([PermissionFlagsBits.MentionEveryone]);
-                    role[1].setPermissions(newRolePerms, "Bot doesn't function corretly when @everyone can mention all roles");
-                };
-                continue;
-            };
-            if(role[1].managed) continue;
-            const roleId = role[1].id;
-
-            roleOutput = roleOutput + `<@&${roleId}>` + `\n`;
-
-            //check if the role already is being timed
-            const timed = await roleInDatabase(roleId);
-            trackingOutput = trackingOutput + inlineCode(timed) + `\n`
-
-            var botPermStatus = "";
-            //check if the bot has a higher role
-
-            if (!botId) return;
-
-            const botUser = await client.guilds.cache.get(guildId).members.cache.get(botId);
-
-            const botRole = await botUser.roles.cache.find(r => r.tags.botId === botId);
-
-            if (!botRole) {
-            interaction.reply({embeds: [deniedMessage("Error! Please report this as a bug")]});
-
-            logging("error", `Could't find managed role. GuildID: ${guildId}`, "check.js/botrole");
-            return;
-            }
+            //Try sending error reply (could fail cause the guild is not availabe)
+            interaction.reply({embeds: [defaultMessage.generalCommandError("Guild not availabe. Possible discord server outage", "err_Check_Unavail_Guild")], ephemeral: true})
+            .catch(error => {
+                logging.globalWarn(__filename, 
+                    logTemplates.commandInteractionException(
+                        interaction, 
+                        "Tried to send guild unavailable interaction reply. Error could be expected.", 
+                        `guildId: ${guildId}, errId: "err_Check_Unavail_Guild", error: ${error}`
+                        )
+                    );
+            });
             
-            const comparedPos = role[1].comparePositionTo(botRole);
-
-            if (comparedPos < 0) {
-                botPermStatus = inlineCode('true') + `\n`;
-            } else {
-                botPermStatus = inlineCode('false - bot rank too low') + `\n`;
-            }
-
-            //check if the bot has required perms
-            if (permsCheck(client, guildId).data.color === 13120512) {
-                botPermStatus = inlineCode('false - not enough perms') + `\n`;
-            }
-
-            canChangeOutput = canChangeOutput + botPermStatus;
-        }
-
-        if (!roleOutput) {
-            interaction.reply({embeds: [deniedMessage("There are no roles in this server that can be managed by this bot")], ephemeral: true});
             return;
         }
 
+
+        //Get the rest of all required vars
+        const clientUser = guildObject.members.me;
+        const clientId = clientUser.id;
+
+        const everyoneRole = guildObject.roles.everyone;
+        const everyoneRoleId = everyoneRole.id;
+
+        const guildRoles = await guildObject.roles.fetch()
+        .catch((error) => {
+            logging.error(__filename, logTemplates.commandInteractionException(interaction, "Error while fetching roles from guild", `code: "err_check_rolefetch", error: "${error}"`));
+
+            interaction.reply({embeds: [defaultMessage.generalCommandError("Error fetching guild roles", "err_check_rolefetch")]})
+            .catch((error) => {
+                logging(__filename, logTemplates.commandInteractionException(interaction, "Error when replying to interaction", `code: "err_int_reply/err_check_rolefetch", error: "${error}"`));
+
+                return;
+            });
+
+            return;
+        })
+
+        const clientRole = guildRoles.find((role) => role.tags?.botId === clientId);
+
+        const botHasRequiredPerms = clientUser.permissions.has(PermissionFlagsBits.ManageRoles, PermissionFlagsBits.SendMessages); //TODO: Setup correct perms (maybe use config file for it?)
+
+        //Return all roles that aren't managed
+        function getUnmanagedRoles(guildRoles) {
+
+            var unmanagedRolesArray = []
+
+            guildRoles.forEach((value) => {
+
+                if (value.managed) return;
+                if (value.id === everyoneRoleId) return;
+
+                unmanagedRolesArray.push(value);
+            })
+
+            return unmanagedRolesArray;
+        }
+
+        const eligibleRoles = getUnmanagedRoles(guildRoles);
+
+        //Check if there are any roles left that could be eligible
+        if (!eligibleRoles) {
+            logging.globalInfo(__filename, logTemplates.commandInteractionInfo(interaction, `Stopping command, guild doesn't have any roles that could be eligible for a timeout role. code: "inf_check_noEligibleRoles";`));
+        
+            interaction.reply({emebds: deniedMessage("No eligible role(s) found")})
+            .catch((error) => logging.warn(__filename, logTemplates.commandInteractionException(interaction, "Error while sending no eligible role(s) found message.", `code: "err_check_noEligibleRoles"`)))
+        }
+
+
+        //Check if all required perms are set for a role
+        function roleCompatibilityCheck (botHasRequiredPerms, clientRole, compareRole) {
+            //Check if the bot has all the required perms to do it's functions.
+            if (!botHasRequiredPerms) {
+                return "False - Not enough perms";
+            }
+
+            //Compare the bot's role with another role. Pos = botRole higher, min = botRole lower, equal = same role.
+            const comparedRolePos = clientRole.comparePositionTo(compareRole)
+
+            if (comparedRolePos < 0) {
+                return "False - Bot has role lower"
+            }
+
+            //If nothing else returned ealier it ok to manage
+            return "True";
+        }
+
+
+        // Data to use in the embed fields
+        var formattedRolesArray = [];
+        var roleInDatabaseArray = [];
+        var canManageRoleArray = [];
+
+        for (const role of eligibleRoles) {
+            const roleId = role.id;
+
+            // Format to a correct role tag and add to it the array
+            const formattedRole = `<@&${roleId}>`;
+            formattedRolesArray.push(formattedRole);
+
+
+            // Check if role is in the database
+            await roleInDatabase(roleId)
+            .then((result) => {
+                roleInDatabaseArray.push(result);
+            })
+            .catch((error) => {
+                roleInDatabaseArray.push(error);
+            });
+
+
+            //Check if role can be edited
+            canManageRoleArray.push(roleCompatibilityCheck(botHasRequiredPerms, clientRole, role));
+
+
+        }
+        
+
+        //Embed Code
+
+        //Transform the array you get with data to a string with every array value being a new line
+        function arrayToNewlineString (array) {
+            const string = array.toString();
+            const newLineString = string.replace(/,/g, "\n");
+
+            return newLineString;
+        }
+
+
+        // Build the emed using the buildIn discordjs embed builder.
         const embed = new EmbedBuilder()
         .setTitle("Available roles")
-        .setFields(
-            {name: 'Role(s)', value: roleOutput, inline: true},
-            {name: 'Timed Role', value: trackingOutput, inline: true},
-            {name: 'Bot Can Manage', value: canChangeOutput, inline: true},
+        .addFields(
+        {name: "Role", value: arrayToNewlineString(formattedRolesArray), inline: true},
+        {name: "Timed Role", value: arrayToNewlineString(roleInDatabaseArray), inline: true},
+        {name: "Bot Can Manage", value: arrayToNewlineString(canManageRoleArray), inline: true}
         )
         .setTimestamp();
 
-        await client.guilds.fetch();
-        const guild = await client.guilds.cache.get(guildId);
-        await guild.members.fetch();
-        const user = await guild.members.cache.get(interaction.user.id)
-        const userPresence = user.presence?.clientStatus;
-        if (userPresence?.mobile) {
-            embed.data.footer = {text: "It has been detected that you are using a mobile device. This embed may not show up correctly on your device. Consider using discord on a computer"};  
-        };
+        interaction.reply({embeds: [embed]})
+        .catch((error) => logging.error(__filename, logTemplates.commandInteractionException(interaction, "Error while sending interaction reply", `Error: "${error}"`)));
 
-        interaction.reply({embeds: [embed], ephemeral: true});
-    },
+
+
+
+
+    }
 };
