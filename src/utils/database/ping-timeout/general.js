857968bfa7312167a70bf721c5c3fa225178c688
SixieP <60109474+SixieP@users.noreply.github.com>
2024-01-07 14:16:54 +0100

    reworked roleInDatabase function to use promise

--- src/utils/database/ping-timeout/general.js
+++ src/utils/database/ping-timeout/general.js
@@ -4,29 +4,39 @@ const connectDatabase = require('../connectDatabase');
 const pool = connectDatabase();
 const promisePool = pool.promise();
 
-async function roleInDatabase(interaction, roleId) {
-    logging.verboseInfo(__filename, `roleInDatabase, interactionId: ${interaction.id} | Query executed`);
-    try {
-        const [ role ] = await promisePool.execute(`
+function roleInDatabase(roleId) {
+    return new Promise(function (resolve, reject) {
+        promisePool.execute(`
         select * from roles
         where
-        roleId = ?`, [roleId]);
+        roleId = ?`, [roleId])
+        .then(([rows, fields]) => {
+            if (rows.length === 1) {
+                resolve("true");
+            } else if (rows.length > 1) {
+                logging.warn(__filename, `roleId more than once in roles table. roleId: "${roleId}"`);
+                reject("error: err_5");
+            } else {
+                resolve("false");
+            }
+        })
+        .catch((error) => {
+            if (error.code === "ECONNREFUSED") {
+                logging.warn(__filename, `Database connection error. Code: "err_datab_nocon-roleInDatabase", errCode: "${error.code}"`);
+            } else {
+                logging.error(__filename, `Unkown database error. Code: "err_datab_unkown-roleInDatabase", errorCode: "${error.code}, error: "${error}""`);
+            }
 
-        if (role[0]) {
-            return true;
-        } else {
-            return false;
-        }
-    } catch (error) {
-        if (error.code === "econnrefused") {
-            logging.error(__filename, `roleInDatabase, interactionId: ${interaction.id} | Error connecting to database: ${error}`);
-            return "err_ECONNREFUSED";
-        } else {
-            logging.error(__filename, ` roleInDatabase, interactionId: ${interaction.id} | There was an issue executing a database query: ${error}`);
-            return "err_error";
-        }
-    }
+            try {
+                reject(error.code)
+            } catch (error) {
+                console.log(error);
+            }
+        });
+    })
 }
+
+
 
 //make functions global
 module.exports = { 
