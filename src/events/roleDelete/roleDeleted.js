361791e06b4e12dcb10e550ebfaf1c0a8cc2b708
SixieP <60109474+SixieP@users.noreply.github.com>
2024-01-20 14:03:04 +0100

    fixed code to use new processes

--- src/events/roleDelete/roleDeleted.js
+++ src/events/roleDelete/roleDeleted.js
@@ -1,21 +1,24 @@
 const { roleInDatabase } = require("../../utils/database/ping-timeout/general");
-const { removeTimeoutRole } = require("../../utils/database/ping-timeout/roleCommand");
+const { removeTimedRoleQuery } = require("../../utils/database/ping-timeout/roleCommand");
 
 const logging = require('../../utils/baseUtils/logging');
 
-module.exports = async (client, role) => {
+module.exports = (client, role) => {
     const roleId = role.id;
 
-    timedRole = await roleInDatabase("", roleId);
+    roleInDatabase(roleId)
+    .then((result) => {
+        if (result) {
+            removeRole()
+        };
+    })
+    .catch((error) => logging.error(__filename, `Error getting role from database. code: "err_datab_getRole", error: "${error}"`));
 
-    if (!timedRole) return;
+    return; //End of normal code. Rest will be below in functions
 
-    logging.globalInfo(__filename, `Ping TimeOut role got deleted from guild. Going to try to remove it from the database. roleId: ${roleId}`);
-
-    const databaseResponse = removeTimeoutRole(roleId);
-    if (databaseResponse === "success") {
-        logging.globalInfo(__filename, `Role removed from database. roleId: ${roleId}`);
-    } else {
-        logging.error(__filename, `Couldn't remove role from database. roleId: ${roleId}, error: ${databaseResponse}`);
+    function removeRole() {
+        removeTimedRoleQuery(roleId)
+        .then(() => logging.globalInfo(__filename, `Successfully remove a role that got remove from discord. roleId: ${roleId}`))
+        .catch((error) => logging.error(__filename, `Error removing role that got removed from discord. error: ${error}`));
     };
-}
+};
