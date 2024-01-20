3d25907149aa4b082007fee39f4e936ac7edc592
SixieP <60109474+SixieP@users.noreply.github.com>
2024-01-20 13:49:21 +0100

    reworked to use new logging function, better error handling and overall better performance. Also fixed page bug with the guilds stat embed

--- src/utils/database/DEV/stats.js
+++ src/utils/database/DEV/stats.js
@@ -5,72 +5,98 @@ const pool = connectDatabase();
 const promisePool = pool.promise();
 
 //get all the roles
-async function statGetAllRoles(interaction) {
-    logging.verboseInfo(__filename, `statGetAllRoles, interactionId: ${interaction.id} | Query executed`);
-    try {
-        const [ role ] = await promisePool.execute(`
+async function statGetAllRoles() {
+    logging.verboseInfo(__filename, 'Executing "statGetAllRoles" function and query');
+
+    return new Promise(function (resolve, reject) {
+        promisePool.execute(`
         select * from roles;
         `)
+        .then(([results]) => {
+            logging.verboseInfo(__filename, 'Successfully executed "statGetAllRoles" query');
 
-        return role;
-    } catch (error) {
-        if (error.code === "econnrefused") {
-            logging.error(__filename, `statGetAllRoles, interactionId: ${interaction.id} | Error connecting to database: ${error}`);
-            return "err_ECONNREFUSED";
-        } else {
-            logging.error(__filename, ` statGetAllRoles, interactionId: ${interaction.id} | There was an issue executing a database query: ${error}`);
-            return "err_error";
-        }
-    }
-}
+            resolve(results);
+        })
+        .catch((error) => {
+            logging.error(__filename, `Error executing "statGetAllRoles" query. code": "err_datab_getRoles", errCode: "${error.code}", error: "${error}"`);
+
+            reject(error);
+        });
+    });
+};
+
+//get athe count of all roles
+async function statGetAllRolesCounted() {
+    logging.verboseInfo(__filename, 'Executing "statGetAllRolesCounted" function and query');
+
+    return new Promise(function (resolve, reject) {
+        promisePool.execute(`
+        select count(*) as "totalTimeoutRoles" from roles;
+        `)
+        .then(([results]) => {
+            logging.verboseInfo(__filename, 'Successfully executed "statGetAllRolesCounted" query');
+
+            resolve(results);
+        })
+        .catch((error) => {
+            logging.error(__filename, `Error executing "statGetAllRolesCounted" query. code": "err_datab_getRoles", errCode: "${error.code}", error: "${error}"`);
+
+            reject(error);
+        });
+    });
+};
 
 //get all roles from a specific guild
-async function statGetRolesByGuild(interaction, guildId) {
-    logging.verboseInfo(__filename, `statGetRolesByGuild, interactionId: ${interaction.id} | Query executed`);
-    try {
-        const [ role ] = await promisePool.execute(`
+async function statGetRolesByGuild(guildId) {
+    logging.verboseInfo(__filename, 'Executing "statGetRolesByGuild" function and query');
+
+    return new Promise(function (resolve, reject) {
+        promisePool.execute(`
         select * from roles
         where
         guildId = ?;
         `, [guildId])
+        .then(([results]) => {
+            logging.verboseInfo(__filename, 'Successfully executed "statGetRolesByGuild" query');
 
-        return role;
-    } catch (error) {
-        if (error.code === "econnrefused") {
-            logging.error(__filename, `statGetRolesByGuild, interactionId: ${interaction.id} | Error connecting to database: ${error}`);
-            return "err_ECONNREFUSED";
-        } else {
-            logging.error(__filename, ` statGetRolesByGuild, interactionId: ${interaction.id} | There was an issue executing a database query: ${error}`);
-            return "err_error";
-        }
-    }
-}
+            resolve(results);
+        })
+        .catch((error) => {
+            logging.error(__filename, `Error executing "statGetRolesByGuild" query. code": "err_datab_getRoles_byGui", errCode: "${error.code}", error: "${error}"`);
+
+            reject(error);
+            return;
+        });
+    });
+};
 
 //get all roles from a specific guild
-async function statGetRole(interaction, roleId) {
-    logging.verboseInfo(__filename, `statGetRole, interactionId: ${interaction.id} | Query executed`);
-    try {
-        const [ role ] = await promisePool.execute(`
+async function statGetRole(roleId) {
+    logging.verboseInfo(__filename, 'Executing "statGetRole" function and query');
+
+    return new Promise(function (resolve, reject) {
+        promisePool.execute(`
         select * from roles
         where
         roleId = ?;
         `, [roleId])
+        .then(([results]) => {
+            logging.verboseInfo(__filename, 'Successfully executed "statGetRole" query');
 
-        return role;
-    } catch (error) {
-        if (error.code === "econnrefused") {
-            logging.error(__filename, `statGetRole, interactionId: ${interaction.id} | Error connecting to database: ${error}`);
-            return "err_ECONNREFUSED";
-        } else {
-            logging.error(__filename, ` statGetRole, interactionId: ${interaction.id} | There was an issue executing a database query: ${error}`);
-            return "err_error";
-        }
-    }
-}
+            resolve(results);
+        })
+        .catch((error) => {
+            logging.error(__filename, `Error executing "statGetRole" query. code": "err_datab_getRole", errCode: "${error.code}", error: "${error}"`);
+
+            reject(error);
+        });
+    });
+};
 
 //make functions global
 module.exports = { 
     statGetAllRoles,
+    statGetAllRolesCounted,
     statGetRolesByGuild,
     statGetRole,
 };
