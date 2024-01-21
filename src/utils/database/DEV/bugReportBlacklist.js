516f919c5ecd4f3c9a4be05ddbd4527b1475fa8d
SixieP <60109474+SixieP@users.noreply.github.com>
2024-01-21 19:26:36 +0100

    updated to use logging/promises

--- src/utils/database/DEV/bugReportBlacklist.js
+++ src/utils/database/DEV/bugReportBlacklist.js
@@ -6,73 +6,79 @@ const promisePool = pool.promise();
 
 //add a user to the blacklist
 async function addUserToBlacklist(interaction, userId, date, reason) {
-    logging.verboseInfo(__filename, `addUserToBlacklist, interactionId: ${interaction.id} | Query executed`);
-    try {
-        const [ result ] = await promisePool.execute(`
+    logging.verboseInfo(__filename, 'Executing "blacklistedUser" function and query');
+
+    return new Promise(function (resolve, reject) {
+        promisePool.execute(`
         insert into bugReportBlacklist
         (userId, blacklistDate, blacklistReason)
         values
         (?, ?, ?)
         `,
         [userId, date, reason,])
+        .then(([results]) => {
+            logging.verboseInfo(__filename, 'Successfully executed "blacklistedUser" query');
 
-        return result;
-    } catch (error) {
-        if (error.code === "econnrefused") {
-            logging.error(__filename, `addUserToBlacklist, interactionId: ${interaction.id} | Error connecting to database: ${error}`);
-            return "err_ECONNREFUSED";
-        } else {
-            logging.error(__filename, ` addUserToBlacklist, interactionId: ${interaction.id} | There was an issue executing a database query: ${error}`);
-            return "err_error";
-        }
-    }
-}
+            resolve(results);
+        })
+        .catch((error) => {
+            logging.error(__filename, `Error executing "blacklistedUser" query. code": "err_datab_getRoles_byGui", errCode: "${error.code}", error: "${error}"`);
+
+            reject(error);
+            return;
+        });
+    });
+};
 
 //remove a user from the blacklist
 async function removeUserFromBlacklist(interaction, userId) {
-    logging.verboseInfo(__filename, `removeUserFromBlacklist, interactionId: ${interaction.id} | Query executed`);
-    try {
-        const [ result ] = await promisePool.execute(`
+    logging.verboseInfo(__filename, 'Executing "blacklistedUser" function and query');
+
+    return new Promise(function (resolve, reject) {
+        promisePool.execute(`
         delete from bugReportBlacklist
         where
         userId = ?
         `,
         [userId])
+        .then(([results]) => {
+            logging.verboseInfo(__filename, 'Successfully executed "blacklistedUser" query');
 
-        return result;
-    } catch (error) {
-        if (error.code === "econnrefused") {
-            logging.error(__filename, `removeUserFromBlacklist, interactionId: ${interaction.id} | Error connecting to database: ${error}`);
-            return "err_ECONNREFUSED";
-        } else {
-            logging.error(__filename, ` removeUserFromBlacklist, interactionId: ${interaction.id} | There was an issue executing a database query: ${error}`);
-            return "err_error";
-        }
-    }
-}
+            resolve(results);
+        })
+        .catch((error) => {
+            logging.error(__filename, `Error executing "blacklistedUser" query. code": "err_datab_getRoles_byGui", errCode: "${error.code}", error: "${error}"`);
+
+            reject(error);
+            return;
+        });
+    });
+};
 
 //remove a user from the blacklist
-async function blacklistedUser(interaction, userId) {
-    logging.verboseInfo(__filename, `blacklistedUser, interactionId: ${interaction.id} | Query executed`);
-    try {
-        const [ userInfo ] = await promisePool.execute(`
+async function blacklistedUser(userId) {
+    logging.verboseInfo(__filename, 'Executing "blacklistedUser" function and query');
+
+    return new Promise(function (resolve, reject) {
+        promisePool.execute(`
         select * from bugReportBlacklist
         where
         userId = ?;
         `,
         [userId])
+        .then(([results]) => {
+            logging.verboseInfo(__filename, 'Successfully executed "blacklistedUser" query');
 
-        return userInfo[0];
-    } catch (error) {
-        if (error.code === "econnrefused") {
-            logging.error(__filename, `blacklistedUser, interactionId: ${interaction.id} | Error connecting to database: ${error}`);
-            return "err_ECONNREFUSED";
-        } else {
-            logging.error(__filename, ` blacklistedUser, interactionId: ${interaction.id} | There was an issue executing a database query: ${error}`);
-            return "err_error";
-        }
-    }
-}
+            resolve(results);
+        })
+        .catch((error) => {
+            logging.error(__filename, `Error executing "blacklistedUser" query. code": "err_datab_getRoles_byGui", errCode: "${error.code}", error: "${error}"`);
+
+            reject(error);
+            return;
+        });
+    });
+};
 
 //make functions global
 module.exports = { 
