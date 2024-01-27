271cd2d5890d5783d0947fe88241dce6c570ab82
SixieP <60109474+SixieP@users.noreply.github.com>
2024-01-27 12:20:32 +0100

    rewrote the code

--- src/utils/ping-timeout/makeMentionable.js
+++ src/utils/ping-timeout/makeMentionable.js
@@ -2,56 +2,99 @@ const logging = require("../baseUtils/logging");
 const { getNotMentionableRoles, databaseRoleErrorState, updateSetMentionable } = require("../database/ping-timeout/makeMentionable");
 const noPermsMessage = require("./noPermsMessage");
 
-module.exports = async (client) => {
-    const roles = await getNotMentionableRoles("", "");
+const { removeAllGuildRoles } = require("../database/guildDelete/removeRoles");
 
-    if (roles === "err_ECONNREFUSED" || roles === "err_error") {
-        logging.error(__filename, "Database error while getting not mentionabel roles.");
-        return;
+const groupBy = require('lodash/groupBy');
+
+module.exports = async (client) => {
+
+    const curDate = new Date();
+    const curDateMs = Date.parse((curDate)?.toUTCString());
+    const curDateSec = curDateMs/1000;
+    
+    getNotMentionableRoles()
+    .then((notMentionableRoles) => sortRolesByGuild(notMentionableRoles))
+    .catch((error) => logging.error(__filename, `Error while getting not mentionable roles. error: "${error}"`));
+
+    function sortRolesByGuild(notMentionableRoles) {
+        const groupedRoles = groupBy(notMentionableRoles, 'guildId');
+
+        const groupedRolesValue = Object.values(groupedRoles);
+
+        for (const roles of groupedRolesValue) {
+            const guildId = roles[0].guildId;
+            client.guilds.fetch(guildId)
+            .then((guildObject) => {
+                for (const role of roles) {
+                    const roleId = role.roleId;
+                    const timeoutTime = role.timeoutTime;
+                    const timeoutTimeSec = timeoutTime*60;
+                    const lastMentioneDate = role.lastMention;
+
+                    const mentioneDateMs = Date.parse((lastMentioneDate)?.toUTCString());
+                    const mentioneDateSec = mentioneDateMs/1000;
+
+                    const timeoutExpireTime = mentioneDateSec+timeoutTimeSec;
+                    const timeoutRestSec = 60 - timeoutExpireTime % 60;
+                    const roundedExpireTime = timeoutRestSec + timeoutExpireTime;
+                
+                    if (curDateSec > (roundedExpireTime - 1)) {
+                        guildObject.roles.fetch(roleId)
+                        .then((roleObject) => {
+                            makeRoleMentionable(roleObject)
+                        })
+                        .catch((error) => logging.error(__filename, `Error while fetching role. code: "err_rolefetch", guildId: "${guildId}", roleId: "${roleId}", error: "${error}"`));
+                    }; 
+                };
+            })
+            .catch((error) => {
+                if (error.code === 10004) {
+                    logging.globalWarn(__filename, `guild not found during fetching. Meaning it doesn't exist anymore, deleting all roles from this guild. guildId: "${guildId}", error: "${error}"`)
+
+                    removeAllGuildRoles(guildId)
+                    .catch((error) => logging.error(__filename, `Error while deleting all roles from a guild that is gone. code: "err_datab_dellAllRoles", guildId: "${guildId}", error: "${error}"`));
+                } else {
+                    logging.error(__filename, `Error while fetching guild. code: "err_guildfetch", guildId: "${guildId}", error: "${error}"`);
+                };
+            });
+        };
     };
 
-    for (const role of roles) {
-        const mentionDate = Date.parse((role.lastMention).toUTCString());
-        const timeNow = Date.parse(new Date().toUTCString());
-        const timePassedMs = timeNow-mentionDate;
-        const timePassedSec = timePassedMs/1000;
-        const timePassedMin = timePassedSec/60;
-        
-        const timeOutTime = role.timeoutTime;
-        const guildId = role.guildId;
-        const roleId = role.roleId
-        const errorState = role.inError;
+    function makeRoleMentionable(roleObject) {
+        const roleId = roleObject.id;
+        const guildId = roleObject.guild.id;
 
-        if(timePassedMin > timeOutTime) {
-            logging.globalInfo(__filename, `Role timeout expired. roleId: ${roleId}`)
-                client.guilds.cache.get(guildId).roles.cache.get(roleId)
-                .then(role => role.setMentionable(true, "Timeout expired, role can be mentioned by everyone again."))
-                .then(() => {
-                    logging.globalInfo(__filename, `Made role mentionable again after timeout expired. roleId: ${role.id}`);
-                })
-                .then(roleId => {
-                    logging.globalInfo(__filename, `Set role to mentionable in database. roleId: ${roleId}`);
-                    updateSetMentionable(roleId);
-                })
-                .catch(error => {
-                    if (error.code === 10011) { //Unknown role
-                        logging.globalWarn(__filename, logTemplate.messageInteractionCustomInfo(message, 'Known error "Unkown role" while: catching a role by id/making it mentionable;', `guildId: ${guildId}, roleId: ${roleId}, error: ${error}`));
-                        return;
-                    } else if (error.code === 50013) { //You lack permissions to perform that action
-                        logging.globalWarn(__filename, logTemplate.messageInteractionCustomInfo(message, 'Known error "You lack permissions to perform that action" while making a role mentionable;', `guildId: ${guildId}, roleId: ${roleId}, error: ${error}`));
+        roleObject.setMentionable(true, "Timeout expired. Can be mentioned by everyone again.")
+        .then(() => {
+            logging.globalInfo(__filename, `Made a role mentionable gain. roleId: ${roleId}`);
 
-                        //Set the role in error state if it wasn't already
-                        if (errorState === 0) {
-                            databaseRoleErrorState(roleId, true);
-                            noPermsMessage(client, roleId, guildId);
-                        };
+            databaseRoleErrorState(roleId, false)
+            .catch((error) => logging.error(__filename, `Error while updating a role's error state in the database. roleId: "${roleId}", error: "${error}"`));
 
-                        return;
-                    } else { //When a not expected error happens
-                        logging.warn(__filename, logTemplate.messageInteractionCustomInfo(message, "Unexpected error while making role mentionable", `guildId: ${guildId}, roleId: ${roleId}, error: ${error}`))
-                        return;
-                    }
-                });;      
-        };
-    }
-}
+            updateRoleInDatabase(roleId, roleObject)
+        })
+        .catch((error => {
+            if (error.code === 50013) {
+                noPermsMessage(client, roleId, guildId);
+            } else {
+                logging.error(__filename, `Error while making a role mentionable. code: "err_role_updaMentio", guildId: "${guildId}", roleId: "${roleId}", error: "${error}"`);
+            };
+
+            databaseRoleErrorState(roleId)
+            .catch((error) => logging.error(__filename, `Error while updating a role's error state in the database. roleId: "${roleId}", error: "${error}"`));
+        }))
+    };
+
+    function updateRoleInDatabase(roleId, roleObject) {
+        updateSetMentionable(roleId)
+        .then(() => logging.verboseInfo(__filename, `Successfully made this role mentionable and edited the database. roleId: "${roleId}"`))
+        .catch((error) => {
+            logging.error(__filename, `Error while updating role in database to mentionable. code: "err_datab_updRol", roleId: "${roleId}"`);
+
+            //LATER: Give error to server that this role won't function until this is resolved.
+
+            databaseRoleErrorState(roleId)
+            .catch((error) => logging.error(__filename, `Error while updating a role's error state in the database. roleId: "${roleId}", error: "${error}"`));
+        });
+    };
+};
