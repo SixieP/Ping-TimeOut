0a461f151d6f8cdef293d036f71899b996426120
SixieP <sixiep@sixie.nl>
2026-03-10 11:08:43 +0100

    directly use role object from message instead of fetching the guild and role separately.

--- src/events/messageCreate/handleRoleMentions.js
+++ src/events/messageCreate/handleRoleMentions.js
@@ -3,7 +3,6 @@ const { updateLastMentionQuery } = require('../../utils/database/ping-timeout/ne
 
 const { databaseRoleErrorState, } = require("../../utils/database/ping-timeout/makeMentionable");
 
-
 const logging = require('../../utils/baseUtils/logging');
 const logTemplate = require('../../utils/baseUtils/logTemplates');
 
@@ -22,38 +21,29 @@ module.exports = async (client, message) => {
 
     const mentionedRoles = message.mentions.roles;
 
-
-
     for (const [key, value] of mentionedRoles) {
-        const roleId = value.id;
-
-        roleInDatabase(roleId)
+        roleInDatabase(key)
         .then((inDatabase) => {
             if (inDatabase) {
-                processRole(roleId)
+                processRole(value)
             }
         })
         .catch((error) => logging.error(__filename, `Error while checking if role is in database. roleId: "${roleId}", error: "${error}"`));
-    };
+    }
 
     return; //End of normal code 
 
-    function processRole(roleId) {
+    function processRole(role) {
         logging.globalInfo(__filename, `A Timeout role got mentioned. roleId: ${roleId}`);
 
-        
-
-        client.guilds.fetch(guildId)
-        .then((guildObject) => guildObject.roles.fetch(roleId))
-        .then((roleObject) => makeRoleUnMentionable(roleObject))
-        .catch((error) => logging.error(__filename, `Error fetching guild/role. error: "${error}"`));
+        makeRoleUnMentionable(role);
 
         function makeRoleUnMentionable(roleObject) {
             roleObject.setMentionable(false, "Timeout role got mentioned")
             .then(() => {
                 logging.globalInfo(__filename, `Timeout role got mentioned and made unmentionable. roleId: "${roleId}", guildId: "${guildId}"`);
 
-                updateLastMentionQuery(roleId)
+                updateLastMentionQuery(roleObject.id)
                 .then(() => logging.globalInfo(__filename, `Timeout role got mentioned, made unmentionable and database got updated. roleId: "${roleId}", guildId: "${guildId}"`))
                 .catch((error) => {
                     logging.error(__filename, `Error while updating role in database, trying to make role mentionable again. roleId: "${roleId}", guildId: "${guildId}", error: "${error}"`);
@@ -63,7 +53,7 @@ module.exports = async (client, message) => {
             })
             .catch(async (error) => {
                 var inError = 0;
-                await statGetRole(roleId)
+                await statGetRole(roleObject.id)
                 .then((result) => {
                     inError = result[0].inError
                 });
@@ -72,21 +62,21 @@ module.exports = async (client, message) => {
                     logging.globalWarn(__filename, `Tried making a unknown role unmentionable. error: ${error}`);
                 } else if (error.code === 50013) {
                     logging.globalInfo(__filename, `Tried making a role unmentionable but the bot doesn't have the perms to do that. roleId: "${roleId}", guildId: "${guildId}"`)
-                    	if(!inError) noPermsMessage(client, roleId, guildId, message);
+                    	if(!inError) noPermsMessage(client, roleObject.id, guildId, message);
                 } else {
                     logging.error(__filename, `Error while making role unmentionable. roleId: "${roleId}", guildId: "${guildId}", error: "${error}"`);
                 };
 
                 if (!inError) {
-                    databaseRoleErrorState(roleId)
+                    databaseRoleErrorState(roleObject.id)
                     .catch((error) => logging.error(__filename, `Error while updating a role's error state in the database. roleId: "${roleId}", error: "${error}"`));
                 }
             });
-        };
+        }
 
         function makeRoleMentionable(roleObject) { //To make the role mentionable again in case that the database query fails.
             roleObject.setMentionable(true, "Remade role mentionable due to a database error preveting the bot to work")
             .catch((error) => logging.error(__filename, `Error while remaking a role mentionable due to a database error. roleId: "${roleId}", guildId: "${guildId}", error: "${error}"`));
-        };
-    };
+        }
+    }
 };
